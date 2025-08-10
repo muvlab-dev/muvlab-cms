663f34de5ce794a6a5816b23f1de35ac4e349538
lukasztukindorf <lt27037@gmail.com>
2025-08-10 17:04:40 +0200

    added data

---
+++ src/api/class-type/controllers/class-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * class-type controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::class-type.class-type');
---
+++ src/api/class-type/services/class-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * class-type service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::class-type.class-type');
