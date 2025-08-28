70cee9af6a215cfcb5b930a3d2e95697cd5ffe27
lukasztukindorf <lt27037@gmail.com>
2025-08-28 10:43:56 +0200

    added events

---
+++ src/api/widget-event/routes/widget-event.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * widget-event router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::widget-event.widget-event');
---
+++ src/api/widget-event/services/widget-event.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * widget-event service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::widget-event.widget-event');
