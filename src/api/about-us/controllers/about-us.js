4997e64362cf1387f704b6e3645cb9cc6760b99d
lukasztukindorf <lt27037@gmail.com>
2025-09-01 20:30:27 +0200

    minor

--- src/api/about-us/controllers/about-us.js
+++ src/api/about-us/controllers/about-us.js
@@ -1,9 +1,19 @@
 'use strict';
 
-/**
- * about-us controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::about-us.about-us');
+module.exports = createCoreController('api::about-us.about-us', ({ strapi }) => ({
+  async find(ctx) {
+    console.log('🔍 About-us controller - find');
+    const { data, meta } = await super.find(ctx);
+    console.log('✅ About-us controller - data pobrane');
+    return { data, meta };
+  },
+
+  async findOne(ctx) {
+    console.log('🔍 About-us controller - findOne');
+    const { data, meta } = await super.findOne(ctx);
+    console.log('✅ About-us controller - data pobrane');
+    return { data, meta };
+  }
+}));
