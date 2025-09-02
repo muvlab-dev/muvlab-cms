217d15d3d20eb8bb0ac2f82beb2d3dbca8c38c09
lukasztukindorf <lt27037@gmail.com>
2025-09-02 13:50:28 +0200

    added webp

--- config/plugins.js
+++ config/plugins.js
@@ -2,4 +2,14 @@ module.exports = () => ({
   'drag-drop-content-types-strapi5': {
     enabled: true,
   },
+  'webp-converter': {
+    enabled: true,
+    config: {
+      mimeTypes: undefined,
+      options: {
+        quality: 85,
+        // WebP options: https://sharp.pixelplumbing.com/api-output#webp
+      },
+    },
+  },
 });
